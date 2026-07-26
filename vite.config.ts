cde5f2a53669245eeebb9568bbf0f8e728290e26
Alokkumar <alokushwaha881@gmail.com>
2026-07-27 01:18:38 +0530

    remove unwanted dependency

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 
@@ -11,10 +10,10 @@ export default defineConfig({
       server: { entry: "server" },
     }),
     react(),
-    tsconfigPaths(),
   ],
   resolve: {
     alias: { "@": `${process.cwd()}/src` },
+    tsconfigPaths: true,
   },
   server: {
     host: "::",
